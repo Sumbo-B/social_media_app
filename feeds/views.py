25477da0f261e947ff3c832279da2f9f90dce9f7
Sumbo Bello <sumbobello@gmail.com>
2025-08-06 14:21:47 +0100

    added permissions (IsAuthenticatedOrReadOnly) to all views

--- feeds/views.py
+++ feeds/views.py
@@ -1,28 +1,48 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404
 from .models import Feeds
-from rest_framework import generics
-from .serializers import simple_feed_ser
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import simple_feed_ser as SimpleFeedSerializer
 
 
-class UserList(generics.ListAPIView):
+
+
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class FeedListView(generics.ListAPIView):
+    """API view to retrieve list of feeds with pagination."""
     queryset = Feeds.objects.all()
-    serializer_class = simple_feed_ser
+    serializer_class = SimpleFeedSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = None  # You can set a pagination class here if needed
 
 
-# Create your views here.
+# API view for home (example endpoint)
+
+class HomeView(APIView):
+    """API view for home endpoint."""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        """Return a welcome message."""
+        return Response({"message": "Hello"}, status=status.HTTP_200_OK)
 
 
+# API view for feed detail
+
+class FeedDetailView(APIView):
+    """API view to retrieve a feed by id."""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, feed_id, *args, **kwargs):
+        """Retrieve a feed by its ID."""
+        feed = get_object_or_404(Feeds, id=feed_id)
+        serializer = SimpleFeedSerializer(feed)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# def home_views (request, *args, **kwargs):
-#     print(args, kwargs)
-#     return HttpResponse("<h1>Hello</h1>")
+## ...existing code...
 
-# def feed_detail_views (request, feed_id, *args, **kwargs):
-#     try:
-#         obj = Feeds.objects.get(id=feed_id)
-#     except:
-#         raise Http404
-#     return HttpResponse(f"<h1>Hello {feed_id} - {obj.content}</h1>")
 
-  
